ebe924a568b79b4eac8b8774289ea3fcfcd296c5
Ahmed Mohamed <44196068+a7med-m7md@users.noreply.github.com>
2020-12-19 18:47:12 +0200

    Update Strip.py

--- CS21-Science-Day-12/Strip.py
+++ CS21-Science-Day-12/Strip.py
@@ -1,15 +1,15 @@
-n = int(input())
-nums = list(map(int, input().split()))
-sumArray = [0]*(n)
-sumArray[0] = nums[0]
+n=int(input())
+numbers=list(map(int,input().split()))
+ways=0
+sumArray = []
+sumArray.append(numbers[0])
 for i in range(1,n):
-    sumArray[i] = sumArray[i-1]+nums[i]
-
-ways = 0
-
-for i in range(n):
-    if sumArray[i] == 0 and sumArray[i-1] != 0 :
+    sumArray.append(pre[i-1]+numbers[i])
+ 
+for i in range(n-1):
+    if pre[i]==pre[n-1]-pre[i]:
         ways+=1
 print(ways)
+
 
 ## O(N)
